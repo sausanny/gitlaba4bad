bcfa30efbec12c65cc1b4293ceb00f1750f55f46
unknown <Amar>
2024-12-09 19:47:43 +0300

    Ex02

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("213571253 Hello, World!");
+Console.WriteLine("23125361Hello, World!");
--- Ex02/obj/Debug/net8.0/Ex02.AssemblyInfo.cs
+++ Ex02/obj/Debug/net8.0/Ex02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085545401843ac75db5ca53945309d05059d8a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141b6acd8cbaca6c0d19b18a209dd8ba12e664f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
